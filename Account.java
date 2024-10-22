b18e00622972fdbf90f7b6b57a64a0e061f68936
godi-png <eraticsyndicate@gmail.com>
2024-10-21 23:24:21 -0600

    finished abstract methods

--- Account.java
+++ Account.java
@@ -1,3 +1,25 @@
-public class Account {
+public abstract class Account {
+  abstract void deposit(double amount);
+  // this.balance += amount;
 
+  abstract boolean withdraw(double amount);
+  /*
+   * if (amount <= this.balance) {
+   * this.balance -= amount;
+   * return true;
+   * }else
+   * return false;
+   */
+  abstract double getBalance();
+  // return this.balance;
+
+  abstract boolean transfer(Account account, double ammount);
+  /*
+   * if(this.withdraw(ammount)){
+   * account.deposit(ammount);
+   * return true;
+   * } else{
+   * return false;
+   * }
+   */
 }

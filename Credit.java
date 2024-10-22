a6b7de8f060ee8b9d3a150c748c0329b4e6787b3
godi-png <eraticsyndicate@gmail.com>
2024-10-21 23:42:13 -0600

    finished constructor and methods for credit class

--- Credit.java
+++ Credit.java
@@ -1,3 +1,32 @@
-public class Credit {
+public class Credit extends Account {
+  private double creditLimit;
 
+  public Credit() {
+  }
+
+  public Credit(double limit) {
+    this.creditLimit = limit;
+  }
+
+  public void deposit(double ammount) {
+
+  }
+
+  public boolean withdraw(double ammount) {
+    return false;
+  }
+
+  public double getBalance() {
+    return 0.00; // add balance attribute to Account class
+  }
+
+  public boolean transfer(Account account, double ammount){
+    if(this.withdraw(ammount)){
+      account.deposit(ammount);
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
 }

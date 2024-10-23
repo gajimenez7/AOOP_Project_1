49a7a98ac78ea4d437bd3a3f8a8b4319012c2fa6
Juan Meza <jcmeza3@miners.utep.edu>
2024-10-23 13:15:37 -0600

    Update Checking.java

--- Checking.java
+++ Checking.java
@@ -1,3 +1,17 @@
-public class Checking {
+public class Checking extends Account {
 
+    public Checking(String accountNumber, double initialBalance) {
+        this.setAccountNumber(accountNumber);
+        this.deposit(initialBalance);
+    }
+
+    @Override
+    public boolean transfer(Account account, double amount) {
+        if (this.withdraw(amount)) {
+            account.deposit(amount);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }

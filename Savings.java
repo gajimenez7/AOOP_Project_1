5127a3ff2a924897e56649427425726ed25cb00e
godi-png <eraticsyndicate@gmail.com>
2024-10-23 17:34:31 -0600

    merged branches

--- Savings.java
+++ Savings.java
@@ -1,17 +1,16 @@
-public class Saving extends Account {
-    
-    public Saving(String accountNumber, double initialBalance) {
-        this.setAccountNumber(accountNumber);
-        this.deposit(initialBalance);
-    }
+public class Savings extends Account {
+  public Savings(String accountNumber, double initialBalance) {
+    this.setAccountNumber(accountNumber);
+    this.deposit(initialBalance);
+  }
 
-    @Override
-    public boolean transfer(Account account, double amount) {
-        if (this.withdraw(amount)) {
-            account.deposit(amount);
-            return true;
-        } else {
-            return false;
-        }
+  @Override
+  public boolean transfer(Account account, double amount) {
+    if (this.withdraw(amount)) {
+      account.deposit(amount);
+      return true;
+    } else {
+      return false;
     }
+  }
 }

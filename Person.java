efc472889e55f92cfdaf138393dd7c86d8455320
sreyes17 <stephanielinda806@gmail.com>
2024-10-23 22:00:50 -0600

    javadoc

--- Person.java
+++ Person.java
@@ -1,11 +1,18 @@
 
-
+/**creates a person class that has info of customers
+ * @author Stephanie Reyes
+ */
 public class Person extends Customer {
 
+    /**creates a person object
+     */
     public Person(String id, String firstName, String lastName,String dob, String address, String phoneNum){
         super(id, firstName, lastName,dob, address, phoneNum);
     }
 
+    /**displays info about customer
+     * @return void
+     */
     public void showCustomerDetails() {
         System.out.println("Customer ID: " + getID());
         System.out.println("Name: " + getFirstName() + " " + getLastName());

138a4f81c88104dcc5702abd5ef190faa0852a3d
godi-png <eraticsyndicate@gmail.com>
2024-10-23 21:54:45 -0600

    basic console io

--- Customer.java
+++ Customer.java
@@ -2,79 +2,85 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Customer {
-    protected List<Account> accounts;
-    private String id;
-    private String firstName;
-    private String lastName;
-    private String dob;
-    private String address;
-    private String phoneNum;
+  protected List<Account> accounts;
+  private String id;
+  private String firstName;
+  private String lastName;
+  private String dob;
+  private String address;
+  private String phoneNum;
 
-    public Customer(String id, String firstName, String lastName, String dob, String address, String phoneNumber) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.dob = dob;
-        this.address = address;
-        this.phoneNum = phoneNumber;
-        this.accounts = new ArrayList<>();
-    }
-    
-    public abstract void showCustomerDetails();
+  public Customer(String id, String firstName, String lastName, String dob, String address, String phoneNumber) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = dob;
+    this.address = address;
+    this.phoneNum = phoneNumber;
+    this.accounts = new ArrayList<>();
+  }
 
-    
-    public String getFirstName(){
-        return firstName;
-    }
-    public String getLastName(){
-        return lastName;
-    }
-    public String getID(){
-        return id;
-    }
-    public String getDOB(){
-        return dob;
-    }
-    public String getPhoneNum(){
-        return phoneNum;
-    }
-    public String getAddress(){
-        return address;
-    }
-    
+  public abstract void showCustomerDetails();
 
-    public void setFirstName(String firstName){
-        this.firstName = firstName;
-    }
-    public void setLastName(String lastName){
-        this.lastName=lastName;
-    }
-    public void setID(String id){
-        this.id = id;
-    }
-    public void setPhoneNum(String phoneNum){
-        this.phoneNum = phoneNum;
-    }
-    public void setAddress(String address){
-        this.address = address;
-    }
-    public void setDOB(String dob){
-        this.dob = dob;
-    }
-    public void addAccount(Account account){
-        accounts.add(account);
-    }
+  public String getFirstName() {
+    return firstName;
+  }
 
-    public Account getAccount(String acctNum){
-        for(Account temp :accounts){
-            if(temp.getAccountNumber().equals(acctNum)){
-                return temp;
-            }
-        }
-        return null;
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getID() {
+    return id;
+  }
+
+  public String getDOB() {
+    return dob;
+  }
+
+  public String getPhoneNum() {
+    return phoneNum;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void setID(String id) {
+    this.id = id;
+  }
+
+  public void setPhoneNum(String phoneNum) {
+    this.phoneNum = phoneNum;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public void setDOB(String dob) {
+    this.dob = dob;
+  }
+
+  public void addAccount(Account account) {
+    accounts.add(account);
+  }
+
+  public Account getAccount(String acctNum) {
+    for (Account temp : accounts) {
+      if (temp.getAccountNumber().equals(acctNum)) {
+        return temp;
+      }
     }
-
-  
-
+    return null;
+  }
 
 }
--- Person.java
+++ Person.java
@@ -1,17 +1,16 @@
 
-
 public class Person extends Customer {
 
-    public Person(String id, String firstName, String lastName,String dob, String address, String phoneNum){
-        super(id, firstName, lastName,dob, address, phoneNum);
-    }
+  public Person(String id, String firstName, String lastName, String dob, String address, String phoneNum) {
+    super(id, firstName, lastName, dob, address, phoneNum);
+  }
 
-    public void showCustomerDetails() {
-        System.out.println("Customer ID: " + getID());
-        System.out.println("Name: " + getFirstName() + " " + getLastName());
-        System.out.println("Date of Birth: " + getDOB());
-        System.out.println("Address: " + getAddress());
-        System.out.println("Phone Number: " + getPhoneNum());
-    }
-    
+  public void showCustomerDetails() {
+    System.out.println("Customer ID: " + getID());
+    System.out.println("Name: " + getFirstName() + " " + getLastName());
+    System.out.println("Date of Birth: " + getDOB());
+    System.out.println("Address: " + getAddress());
+    System.out.println("Phone Number: " + getPhoneNum());
+  }
+
 }
